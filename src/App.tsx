9ba7e25f2680696c943d8e02c08a59ab6aa78790
faridzam <skinel123@gmail.com>
2023-12-19 22:49:07 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Login from "./app/auth/Login";
 import EmployeePage from "./app/dashboard/employee/EmployeePage";
 import RolePage from "./app/dashboard/role/RolePage";
 import Navbar from "./components/Navbar/Navbar";
+import CustomThemeProvider from "./libs/muiTheme/colorProvider";
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='*' element={<NotFoundPage />} />
-        <Route path="/" element={<Login/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/employee" element={<Navbar><EmployeePage /></Navbar>} />
-        <Route path="/role" element={<Navbar><RolePage /></Navbar>} />
-      </Routes>
-    </BrowserRouter>
+    <CustomThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<NotFoundPage />} />
+          <Route path="/" element={<Login/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/employee" element={<Navbar><EmployeePage /></Navbar>} />
+          <Route path="/role" element={<Navbar><RolePage /></Navbar>} />
+        </Routes>
+      </BrowserRouter>
+    </CustomThemeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import CustomThemeProvider from './libs/muiTheme/colorProvider';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -44,9 +43,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <CustomThemeProvider>
-      <App />
-    </CustomThemeProvider>
+    <App />
   </React.StrictMode>
 );
 

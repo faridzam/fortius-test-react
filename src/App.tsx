ee8ac4088a7d483d5ae913baaf7dae025de55830
faridzam <skinel123@gmail.com>
2023-12-19 21:43:02 +0700

    fix: hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFoundPage from "./app/404/NotFoundPage";
 import Login from "./app/auth/Login";
 import EmployeePage from "./app/dashboard/employee/EmployeePage";
 import RolePage from "./app/dashboard/role/RolePage";
 import Navbar from "./components/Navbar/Navbar";
-import CustomThemeProvider from "./libs/muiTheme/colorProvider";
 
 function App() {
 
   return (
-    <CustomThemeProvider>
-      <HashRouter>
-        <Routes>
-          <Route path='*' element={<NotFoundPage />} />
-          <Route path="/" element={<Login/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/employee" element={<Navbar><EmployeePage /></Navbar>} />
-          <Route path="/role" element={<Navbar><RolePage /></Navbar>} />
-        </Routes>
-      </HashRouter>
-    </CustomThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<NotFoundPage />} />
+        <Route path="/" element={<Login/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/employee" element={<Navbar><EmployeePage /></Navbar>} />
+        <Route path="/role" element={<Navbar><RolePage /></Navbar>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
